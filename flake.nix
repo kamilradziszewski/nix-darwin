31718e01e1c2943a08a3d571d1aead1ac15274c6
Kamil Radziszewski <contact@kamilradziszewski.com>
2025-02-08 13:29:19 +0100

    initial, minimal config

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "nix-darwin flake config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    home-manager,
+    darwin,
+    ...
+  }: let
+    # replace with your own system, username, useremail, and hostname
+    system = "__SYSTEM__"; # aarch64-darwin or x86_64-darwin
+    username = "__USERNAME__";
+    useremail = "__USEREMAIL__";
+    hostname = "__HOSTNAME__";
+  in {
+    darwinConfigurations."${hostname}" = darwin.lib.darwinSystem {
+      inherit system;
+      modules = [
+        {
+          system.stateVersion = 6;
+          nixpkgs.hostPlatform = system;
+          networking.hostName = hostname;
+
+          security.pam.enableSudoTouchIdAuth = true;
+
+          environment.shellAliases = {
+            switch = "darwin-rebuild switch --flake ~/.config/nix-darwin";
+          };
+
+          nixpkgs.config.allowUnfree = true;
+          environment.systemPackages = with nixpkgs.legacyPackages.${system}; [
+            alejandra
+            fzf
+            git
+            oh-my-posh
+            tmux
+            zoxide
+          ];
+        }
+      ];
+    };
+
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+  };
+}
