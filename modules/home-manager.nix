887434a6b4ec01dc81aaef23cfa51b22ea677fc8
Kamil Radziszewski <contact@kamilradziszewski.com>
2025-02-16 13:36:41 +0100

    add VSCode snippets

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -116,6 +116,26 @@ in
             # pkgs.vscode-marketplace.steoates.autoimport
             # pkgs.vscode-marketplace.streetsidesoftware.code-spell-checker
           ];
+
+          languageSnippets = {
+            typescriptreact = {
+              "React Functional Component" = {
+                "prefix" = [ "ffc" ];
+                "body" = [
+                  "import { FC } from \"react\";"
+                  "interface \${TM_FILENAME_BASE}Props {}"
+                  ""
+                  "const \$TM_FILENAME_BASE: FC<\${TM_FILENAME_BASE}Props> = () => {"
+                  "  return <div>null$2</div>"
+                  "};"
+                  ""
+                  "export default \$TM_FILENAME_BASE"
+                  ""
+                ];
+                "description" = "React Functional Component";
+              };
+            };
+          };
         };
       };
     };
